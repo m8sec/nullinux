0603dfdb65adce3ab9471e9dac3f8344eef9fba9
m8r0wn <m897103@gmail.com>
2017-11-06 16:06:08 -0500

    nullinux updates

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+#Check if Script run as root
+if ! [ $(id -u) = 0 ]; then
+	echo -e "\n[!] Setup script needs to run as root.\n\n"
+	exit 0
+fi
+
+echo -e "\n[*] Starting nullinux setup script"
+
+echo -e "[*] Checking for Python 2.7..."
+if [[ $(python2.7 -V 2>&1) == *"not found"* ]]
+then
+    echo -e "[*] Installing Python 2.7"
+    apt-get install python2.7 -y
+else
+    echo "[+] Python 2.7 installed"
+fi
+
+echo -e "[*] Checking for Samba client tools..."
+if [[ $(samba -V 2>&1) == *"not found"* ]]
+then
+    echo -e "[*] Installing Samba client tools"
+    apt-get install samba -y
+else
+    echo "[+] Samba client tools installed"
+fi
+
+echo -e "\n[*] nullinux setup complete\n\n"
